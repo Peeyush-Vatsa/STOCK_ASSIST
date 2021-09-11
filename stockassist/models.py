f2abbdf82c455c8183c5714dba9870b3cee6647b
Peeyush Vatsa <peeyushvatsa@Sanjays-MacBook-Air-2.local>
2021-09-11 18:25:00 +0530

    Creating a database model for current watchlists for a user, adding the model to the admin site and adding the stockassist app to the project

--- stockassist/models.py
+++ stockassist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class watchlist_stocks_current(models.Model):
+    stock_symbol = models.CharField(max_length=50, default='NULL_STOCK')
+    company = models.CharField(max_length=200, default='NULL_STOCK')
+    open = models.FloatField(null=False)
+    PE_ratio = models.CharField(default='null',max_length=20)
+    Mktcap =  models.IntegerField(default=0)
+    Avg_vol = models.IntegerField(default=0)
+    Yield = models.FloatField(default=-100)
+    EPS = models.CharField(default='null',max_length=20)
+    current_price = models.FloatField(null=False)
+    prev_price = models.FloatField(null=False)
+    beta = models.FloatField(null=False, default=-1.0)
+    user_link = models.ManyToManyField(User)
