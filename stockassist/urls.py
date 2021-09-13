3fc71138083e6c5fedbbb0cee2b9ffa1ce848d8e
Peeyush Vatsa <peeyushvatsa@Sanjays-MacBook-Air-2.local>
2021-09-13 17:08:37 +0530

    Creating an authentication and registration view for the app

--- stockassist/urls.py
+++ stockassist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import homepage, signup
+from .views import homepage, login_req, logout_req, signup
 
 app_name = 'stockassist'
 urlpatterns = [
     path('', view=homepage, name='homepage'),
     path('signup/', view=signup, name='signup'),
+    path('login/', view=login_req, name='login'),
+    path('logout/', view=logout_req, name='logout'),
 ]
--- stockassist/views.py
+++ stockassist/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+import django.contrib.auth as auth
 
 # Create your views here.
 def homepage(request):
@@ -10,3 +12,42 @@ def signup(request):
     if request.method == 'GET':
         context = {}
         return render(request, 'stockassist/signup.html', context)
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pwd']
+        firstName = request.POST['fname']
+        lastName = request.POST['lname']
+        emailid = request.POST['emailid']
+        userExist = False
+        try:
+            User.objects.get(username=username)
+            userExist = True
+        except:
+            pass
+        if not userExist:
+            user = User.objects.create_user(username=username, password=password, first_name=firstName, last_name=lastName, email=emailid)
+            auth.login(request, user)
+            return redirect('stockassist:homepage')
+        else:
+            return render(request, 'stockassist/index.html')
+    else:
+        return render(request, 'stockassist/index.html')
+
+def login_req(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pwd']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('stockassist:homepage')
+        else:
+            return render(request, 'stockassist/index.html')
+    else:
+        return render(request, 'stockassist/index.html')
+
+def logout_req(request):
+    if request.method == 'GET':
+        auth.logout(request)
+        return redirect('stockassist:homepage')
+
