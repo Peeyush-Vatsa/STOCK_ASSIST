b94d007701453d9ca668220ad3398c50cd53ebb6
Peeyush Vatsa <peeyushvatsa@Peeyushs-MacBook-Air.local>
2021-11-10 20:31:58 +0530

    Adds support for fetching details for other stocks

--- stockassist/static/stockassist/stockinfo.js
+++ stockassist/static/stockassist/stockinfo.js
@@ -10,6 +10,30 @@ const swap_stock_symbol_reverse = (st) => {
     }
     return stock;
 }
+
+const fetch_stock_info = (stock) => {
+    const stock_short = swap_stock_symbol_reverse(stock);
+    //Add loadbox support
+    $.ajax({
+        type: 'GET',
+        url: '../ajax/getInfo',
+        data: {
+            stock: stock_short,
+        },
+        success: (response) => {
+            $("#stock_info_name").html("<b class='text-info'>"+stock+"</b>");
+            const fundamentals = response.fundamental;
+            for (at in fundamentals){
+                $("#"+at).text(fundamentals[at]);
+            }
+        },
+        error: (error) => {
+            //Add error box
+            console.log(error);
+        }
+    });
+}
+
 $('document').ready(() => {
     setTimeout(() => {
         const stock = $("#stock_info_name").attr('class');
@@ -30,9 +54,6 @@ $('document').ready(() => {
             error: (error) => {
                 //Add error box
                 console.log(error);
-            },
-            complete: (xhr, status) => {
-
             }
         });
     }, 1000);
