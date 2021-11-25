b2e30fd116aaaf010aacc8ae148357fb3d937fcc
Peeyush Vatsa <peeyushvatsa@Peeyushs-MacBook-Air.local>
2021-11-25 18:00:31 +0530

    Adds files to static server

--- static/stockassist/chartassist.js
+++ static/stockassist/chartassist.js
@@ -8,5 +8,63 @@ const plotStockChart = (TIMEPERIOD) => {
             }
         });
         $("#"+TIMEPERIOD).addClass('active').removeClass('text-secondary').addClass('bg-secondary');
+        const stock = swap_stock_symbol_reverse($("#stock_info_name").text());
+        $.ajax({
+            type: 'GET',
+            url: '../ajax/getChartData',
+            data: {
+                stock: stock,
+                period: TIMEPERIOD
+            },
+            success: (response) => {
+                if (TIMEPERIOD == '1D'){
+                    const prevClose = sessionStorage.getItem(stock.replace('{}', '.'));
+                    let prevCloseData = [];
+                    let xDataset = [];
+                    let yDataset = [];
+                    for (let time in response){
+                        xDataset.push(time);
+                        yDataset.push(response[time]);
+                        prevCloseData.push(prevClose);
+                    }
+                    const stock_with_backslash = swap_stock_symbol(stock.replace('{}', '.'));
+                    const stock_direction = $("#watchlist_arrow_"+stock_with_backslash).html();
+                    let colorset = 'rgba(100, 149, 237, 1.0)';
+                    //Changes the color of the stock based on stock direction for the chart
+                    console.log(stock_with_backslash)
+                    if (stock_direction == 'arrow_upward'){
+                        colorset = 'rgba(0,128,0,1.0)';
+                    }
+                    else if (stock_direction == 'arrow_downward'){
+                        colorset = 'rgba(178,34,34,1.0)';
+                    }
+                    intradayChart.destroy();
+                    plotChart(xDataset, yDataset, colorset, prevCloseData);
+                }
+                else{
+                    //For any other timeperiod
+                    let xDataset = [];
+                    let yDataset = [];
+                    for (let time in response){
+                        let date = new Date(Number(time));
+                        xDataset.push(date.getDate().toString() + '/' + date.getMonth().toString()+ '/' + date.getFullYear().toString());
+                        yDataset.push(Number(response[time]).toFixed(2));
+                    }
+                    let colorset = 'rgba(100, 149, 237, 1.0)';
+                    const netDif = yDataset[yDataset.length - 1] - yDataset[0];
+                    if (netDif > 0){
+                        colorset = 'rgba(0,128,0,1.0)';
+                    }
+                    else if (netDif < 0){
+                        colorset = 'rgba(178,34,34,1.0)';
+                    }
+                    intradayChart.destroy();
+                    plotChart(xDataset, yDataset, colorset);
+                }
+            },
+            error: () => {
+                //WRITE LATER
+            }
+        });
     }
 }
